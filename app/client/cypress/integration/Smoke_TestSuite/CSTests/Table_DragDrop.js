34fab1b62f0a4ca681148f90240c521296b0b34c
Aishwarya UR <aishwarya@appsmith.com>
2021-12-22 20:30:19 +0530

    Changing schema script updated

--- app/client/cypress/integration/Smoke_TestSuite/CSTests/Table_DragDrop.js
+++ app/client/cypress/integration/Smoke_TestSuite/CSTests/Table_DragDrop.js
@@ -2,9 +2,11 @@ const widgetsPage = require("../../../locators/Widgets.json");
 const commonlocators = require("../../../locators/commonlocators.json");
 const explorer = require("../../../locators/explorerlocators.json");
 import homePage from "../../../locators/HomePage.json";
+const publish = require("../../../locators/publishWidgetspage.json");
 
 describe("Table Widget DragDrop cases", function() {
   it("1. Table Widget Functionality To Check with changing schema of tabledata", () => {
+    let jsContext = `{{Switch1.isSwitchedOn?[{name: "joe"}]:[{employee_name: "john"}];}}`;
     cy.NavigateToHome();
     cy.get(homePage.createNew)
       .first()
@@ -14,28 +16,21 @@ describe("Table Widget DragDrop cases", function() {
       "response.body.responseMeta.status",
       201,
     );
-
     cy.get(explorer.addWidget).click();
     cy.dragAndDropToCanvas("switchwidget", { x: 200, y: 200 });
     cy.dragAndDropToCanvas("tablewidget", { x: 200, y: 300 });
-    cy.wait(3000);
-    cy.openPropertyPane("tablewidget");
-    cy.widgetText("Table1", widgetsPage.tableWidget, commonlocators.tableInner);
-    cy.testJsontext(
-      "tabledata",
-      `{{
-      Switch1.isSwitchedOn ? [
-          {
-            name: "joe"
-          }
-        ] : [
-          {
-            employee_name: "john"
-          }
-        ];
-    }}`,
-    );
+    cy.wait(1000);
     cy.wait("@updateLayout");
+    //cy.openPropertyPane("tablewidget");
+    //cy.widgetText("Table1", widgetsPage.tableWidget, commonlocators.tableInner);
+    cy.testJsontext("tabledata", jsContext);
+    cy.wait(2000);
+    cy.wait("@updateLayout");
+    // cy.wait("@updateLayout").then(({ response }) => {
+    //   cy.log("Response is :" + JSON.stringify(response.body))
+    //   //expect(response.body.data.dsl.children[1].tableData).to.eq(jsContext);
+    // });
+    cy.wait(2000); //waiting for AutoSave & then publishing!
     cy.PublishtheApp();
     cy.wait(2000);
     cy.getTableDataSelector("0", "0").then((element) => {
@@ -64,5 +59,10 @@ describe("Table Widget DragDrop cases", function() {
     cy.readTabledataPublish("0", "0").then((value) => {
       expect(value).to.be.equal("joe");
     });
+
+    cy.get(publish.backToEditor)
+      .first()
+      .click()
+      .wait(1000);
   });
 });
